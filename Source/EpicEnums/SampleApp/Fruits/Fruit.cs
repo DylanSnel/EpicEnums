7d35023b32b851eac96b6694474ba6322053e8d0
Dylan Snel <dylan@diditsolutions.nl>
2023-07-29 01:08:29 +0200

    some minor updates

--- Source/EpicEnums/SampleApp/Fruits/Fruit.cs
+++ Source/EpicEnums/SampleApp/Fruits/Fruit.cs
@@ -6,23 +6,6 @@ public partial record Fruit : IEpicEnumValue
 {
     public required string Name { get; init; }
     public required string Description { get; init; }
-
-    //public bool IsFruit()
-    //{
-    //    return _FruitsValue is not null;
-    //}
-    //public static implicit operator FruitsEnum(Fruit fruit)
-    //{
-    //    return fruit._FruitsValue ?? throw new UnsupportedValueException();
-    //}
-
-    //public static bool operator ==(Fruit left, FruitsEnum right)
-    //{
-    //    return left._FruitsValue == right;
-    //}
-    //public static bool operator !=(Fruit left, FruitsEnum right)
-    //{
-    //    return left._FruitsValue != right;
-    //}
+    public required bool LikeAble { get; init; }
 
 }
--- Source/EpicEnums/SampleApp/Fruits/Fruits.cs
+++ Source/EpicEnums/SampleApp/Fruits/Fruits.cs
@@ -4,13 +4,9 @@ namespace SampleApp.Fruits;
 
 public partial record Fruits : EpicEnum<Fruit>
 {
-    public static Fruit Apple { get; } = new() { Name = "Apple", Description = "A red fruit" };
-    public static Fruit Banana { get; } = new() { Name = "Banana", Description = "A yellow fruit" };
-    public static Fruit Orange { get; } = new() { Name = "Orange", Description = "An orange fruit" };
-    public static Fruit Papaya { get; } = new() { Name = "Papaya", Description = "An also orange fruit" };
-    public static Fruit DragonFruit { get; } = new() { Name = "Dragon Fruit", Description = "A pink fruit" };
-    public static Fruit Grape { get; } = new() { Name = "Grape", Description = "A purple fruit" };
-    public static Fruit StarFruit { get; } = new() { Name = "Star Fruit", Description = "A yellow fruit" };
-    public static Fruit Durian { get; } = new() { Name = "Durian", Description = "A smelly fruit" };
+    public static Fruit Apple { get; } = new() { Name = "Apple", Description = "A red fruit", LikeAble = true };
+    public static Fruit Banana { get; } = new() { Name = "Banana", Description = "A yellow fruit", LikeAble = true };
+    public static Fruit Orange { get; } = new() { Name = "Orange", Description = "An orange fruit", LikeAble = false };
+    public static Fruit DragonFruit { get; } = new() { Name = "Dragon Fruit", Description = "A pink fruit", LikeAble = true };
 }
 
