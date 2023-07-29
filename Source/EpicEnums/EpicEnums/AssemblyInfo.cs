5de35c49280fafe6096c6f0a250f33454019420e
Dylan Snel <dylan@diditsolutions.nl>
2023-07-29 18:06:14 +0200

    new versioning hopefully

--- Source/EpicEnums/EpicEnums/AssemblyInfo.cs
+++ Source/EpicEnums/EpicEnums/AssemblyInfo.cs
@@ -1,12 +1,18 @@
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // In SDK-style projects such as this one, several assembly attributes that were historically
 // defined in this file are now automatically added during build and populated with
 // values defined in project properties. For details of which attributes are included
 // and how to customise this process see: https://aka.ms/assembly-info-properties
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-
+[assembly: System.Reflection.AssemblyCompanyAttribute("Slyng")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.*")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.*")]
+[assembly: System.Reflection.AssemblyProductAttribute("EpicEnums")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Epic Enums")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
 // Setting ComVisible to false makes the types in this assembly not visible to COM
 // components.  If you need to access a type in this assembly from COM, set the ComVisible
 // attribute to true on that type.
